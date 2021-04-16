34d303b9a125eb7aa7131dfa9aa152089ae1657a
brittanyaylia <whitakerbrittany18@gmail.com>
2021-04-16 14:48:18 -0700

    code

--- app.py
+++ app.py
@@ -4,11 +4,24 @@ from flask import Flask, jsonify
 #Dependencies
 import numpy as np 
 import datetime as datetime
-
-#SQL toolkit
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from sqlalchemy.pool import StaticPool
+
+#Create engine
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+#Reflect existing db & tables
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+#Save references to tables
+measure=Base.classes.measurement
+station = Base.classes.station
+Session = Session(engine)
+
+#Create app
+app = Flask(__name__)
 
