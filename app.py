264fd27f92db9cf4cf0122939d307957f754eb98
brittanyaylia <whitakerbrittany18@gmail.com>
2021-04-17 11:06:45 -0700

    code

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def Welcome():
 
 #Precipitation route
 @app.route("/api/v1.0/precipitation")
-
 def Precipitation():
     one_year = dt.date(2017, 8, 23) - dt.timedelta(days = 365)
     prcp = Session.query(measure.date, measure.prcp).\
@@ -53,10 +52,32 @@ def Precipitation():
 
 #Station route
 @app.route("/api/v1.0/stations")
-
 def Stations():
     stations = Session.query(station.station, station.name).all()
     stations_list = list(stations)
 
     return jsonify(stations_list)
+
+#TOBs route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    one_year = dt.date(2017, 8, 23) - dt.timedelta(days= 365)
+    tobs_data = Session.query(measure.date, measure.tobs).\
+        filter(measure.date >= one_year).\
+        order_by(measure.date).all()
+    tobs_list = list(tobs_data)
+
+    return jsonify(tobs_list)
+
+#Start route
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    start_date = Session.query(measure.date, func.min(measure.tobs), 
+                func.avg(measure.tobs), func.max(measure.tobs)).\
+                    filter(measure.date >= start).\
+                    group_by(measure.date).all()
+    start_list = list(start_date)
+
+    return jsonify(start_list)
+
 
